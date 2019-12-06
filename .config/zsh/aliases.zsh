657e52052a8d4f7f7e0c5963f078667cc08fa0fb
dr1s <dr1s@users.noreply.github.com>
2019-12-06 17:25:25 +0700

    Update zsh aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -2,3 +2,5 @@ alias docker-delete-containers='docker rm $(docker ps -a -q)'
 alias docker-delete-images='docker rmi $(docker images -q)'
 alias docker-delete-images-dangling='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
 alias wttr="curl wttr.in/ho-chi-minh-city"
+alias rgrep="grep -r"
+alias fgrep="grep -F"

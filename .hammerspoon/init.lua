51f217f9e338439f5a2094a5d334c56b8853ba59
dr1s <dr1s@users.noreply.github.com>
2020-07-21 11:23:54 +0700

    Update hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,3 @@
-require "yabai"
-require "skhd"
-
 --- Keyboard layout
 function selectKarabinerProfile(profile)
   hs.execute("'/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --select-profile '" .. profile .. "'")
@@ -11,6 +8,66 @@ function plainInputSourceChange()
   selectKarabinerProfile(isrc)
 end
 hs.keycodes.inputSourceChanged(plainInputSourceChange)
+
+--- SKHD mode menu
+skhd_mode = nil
+skhdmenu = hs.menubar.new()
+function updateSkhdMode()
+  if skhd_mode ~= nil then
+    skhdmenu:setIcon(hs.configdir .. '/assets/' .. skhd_mode .. '.tiff')
+    --skhdmenu:setTitle(mode)
+  else
+    --skhdmenu:setTitle("")
+    skhdmenu:setIcon(hs.configdir .. '/assets/code.tiff')
+    ---skhdmenu:setIcon()
+    ---shkdmebu = nil
+  end
+end
+
+updateSkhdMode()
+
+hs.urlevent.bind("skhd_mode", function(eventName, params)
+  if params["mode"] then
+    skhd_mode = params["mode"]
+  else  
+    skhd_mode = nil
+  end
+  updateSkhdMode()
+end)
+
+--- yabai/hammerspoon menu
+json = require "json"
+yabai = '/usr/local/bin/yabai'
+
+menu = hs.menubar.new()
+
+function yabaiSwitchMode(yabai_mode)
+  if yabai_mode == "bsp" then
+    hs.execute(yabai .. " -m space --layout float")
+  else
+    hs.execute(yabai .. " -m space --layout bsp")
+  end
+  updateMenu()
+end
+
+function updateMenu()
+  yjson = hs.execute(yabai .. " -m query --spaces --space")
+  if yjson ~= '' then
+    space = json.decode(yjson)
+    yabai_mode = space['type']
+    menu:setIcon(hs.configdir .. '/assets/' .. yabai_mode .. '.tiff')
+    menu:setMenu(function() yabaiSwitchMode(yabai_mode) end )
+  else
+    menu:setIcon(hs.configdir .. '/assets/x.tiff')
+  end
+end
+
+updateMenu()
+
+hs.timer.doEvery(30, updateMenu)
+hs.urlevent.bind("cmenu", function(eventName, params)
+  updateMenu()
+end)
 
 --- hammerspoon config
 hs.autoLaunch(true)
